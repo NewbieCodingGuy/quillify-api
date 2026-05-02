edd9df27a94203c44826235aff1080fb3ca46329
NewbieCodingGuy <shekharali302@gmail.com>
2026-05-02 13:50:53 +0530

    bugfix: usageResolver

--- src/modules/usage/usage.resolver.ts
+++ src/modules/usage/usage.resolver.ts
@@ -2,7 +2,7 @@ import { Resolver, Query } from '@nestjs/graphql';
 import { UseGuards } from '@nestjs/common';
 import { UsageService } from './usage.service';
 import { UsageType } from './dto/usage.type';
-import { GqlAuthGuard } from 'src/common/guards/gql-auth.guard';
+import { GqlAuthGuard } from '../../common/guards/gql-auth.guard';
 import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 import type { JwtPayload } from 'src/common/decorators/current-user.decorator';
 import { UserPlan } from '../auth/entities/user.entity';
