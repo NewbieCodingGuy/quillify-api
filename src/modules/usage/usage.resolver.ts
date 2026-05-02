12d5b6c7a92185bdb89612b370cd804f4f21431a
NewbieCodingGuy <shekharali302@gmail.com>
2026-05-02 13:53:29 +0530

    bugfix: usageResolver asking for correct userdecorator path

--- src/modules/usage/usage.resolver.ts
+++ src/modules/usage/usage.resolver.ts
@@ -3,7 +3,7 @@ import { UseGuards } from '@nestjs/common';
 import { UsageService } from './usage.service';
 import { UsageType } from './dto/usage.type';
 import { GqlAuthGuard } from '../../common/guards/gql-auth.guard';
-import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { CurrentUser } from '../../common/decorators/current-user.decorator';
 import type { JwtPayload } from 'src/common/decorators/current-user.decorator';
 import { UserPlan } from '../auth/entities/user.entity';
 
